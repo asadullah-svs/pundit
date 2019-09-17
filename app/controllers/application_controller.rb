0f9446db034df737a0c8da9f531b5d920a27dd5c
zamananjum20 <usama.ashraf.khan@gmail.com>
2019-09-17 16:25:39 +0500

    Integrated pundit successfully and applied to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
   before_action :require_login
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    flash[:warning] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
+    redirect_to(request.referrer || root_path)
+  end
 
 end
